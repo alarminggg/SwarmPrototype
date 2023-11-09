f3c49a915a54729f64ca78dffe23646b28b23ff2
alarminggg <124152367+alarminggg@users.noreply.github.com>
2023-11-09 11:53:47 +0000

    idk

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,95 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
+
+[RequireComponent(typeof(CharacterController))]
 public class PlayerMovement : MonoBehaviour
 {
+    CharacterController characterController;
+    Transform playerContainer, cameraContainer;
+
+    public float speed = 6.0f;
+    public float jumpSpeed = 10f;
+    public float mouseSensitivity = 2f;
+    public float gravity = 20.0f;
+    public float lookUpClamp = -30f;
+    public float lookDownClamp = 60f;
+
+    private Vector3 moveDirection = Vector3.zero;
+
+    [SerializeField] private LayerMask groundMask;
+
+    private Camera mainCamera;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        characterController = GetComponent<CharacterController>();
+
+        mainCamera = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
+        Locomotion();
+        Aim();
         
+    }
+
+    void Locomotion()
+    {
+        if (characterController.isGrounded)//When grounded, set the y-axis to zero(to ignore it)
+        {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= speed;
+
+            if (Input.GetButton("Jump"))
+            {
+                moveDirection.y = jumpSpeed;
+            }
+            if (Input.GetKey(KeyCode.C))
+            {
+                characterController.height = 0.65f;
+                characterController.center = new Vector3(0f, 0.5f, 0f);
+            }
+            else
+            {
+                characterController.height = 1.8f;
+                characterController.center = new Vector3(0f, 0.9f, 0f);
+            }
+        }
+
+        moveDirection.y -= gravity * Time.deltaTime;
+        characterController.Move(moveDirection * Time.deltaTime);
+    }
+
+    private void Aim()
+    {
+        var (success, position) = GetMousePosition();
+        if (success)
+        {
+            var direction = position - transform.position;
+
+            direction.y = 0;
+
+            transform.forward = direction;
+        }
+    }
+    private (bool success, Vector3 position) GetMousePosition()
+    {
+        var ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out var hitInfo, Mathf.Infinity, groundMask))
+        {
+            return (success: true, position: hitInfo.point);
+        }
+        else
+        {
+            return (success: false, position: Vector3.zero);
+        }
     }
 }
